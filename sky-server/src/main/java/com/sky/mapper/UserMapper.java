cdf594fed53c4997d4eeed93e38cb3e4efd4d7ec
kang <you@example.com>
2023-10-28 15:20:07 +0800

    微信支付业务代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
 
 /**
  * @author yao
@@ -14,4 +15,7 @@ public interface UserMapper {
     User getByOpenId(String openId);
 
     Long insert(User user);
+
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
