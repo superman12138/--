646117e217620301f3f5e318368a4bf3873df83d
kang <you@example.com>
2023-10-30 20:24:17 +0800

    订单状态处理和来电提醒业务代码开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,10 +1,12 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
+import com.sky.dto.*;
+import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
+import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
-import org.apache.ibatis.annotations.Mapper;
+import com.sky.vo.OrderVO;
+
 
 /**
  * @author yao
@@ -12,6 +14,8 @@ import org.apache.ibatis.annotations.Mapper;
  */
 
 public interface OrderService {
+    PageResult pageQueryByUser(int page, int pageSize, Integer status);
+
     /**
      * 用户下单
      * @param ordersSubmitDTO
@@ -26,4 +30,60 @@ public interface OrderService {
      * @param outTradeNo
      */
     void paySuccess(String outTradeNo);
+
+    OrderVO details(Long id);
+
+    void userCancelById(Long id) throws Exception;
+
+    void repetition(Long id);
+
+    /**
+     * 条件搜索订单
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 各个状态的订单数量统计
+     * @return
+     */
+    OrderStatisticsVO statistics();
+
+    /**
+     * 接单
+     *
+     * @param ordersConfirmDTO
+     */
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
+
+    /**
+     * 拒单
+     *
+     * @param ordersRejectionDTO
+     */
+    void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
+
+    /**
+     * 商家取消订单
+     *
+     * @param ordersCancelDTO
+     */
+    void cancel(OrdersCancelDTO ordersCancelDTO) throws Exception;
+
+    /**
+     * 派送订单
+     *
+     * @param id
+     */
+    void delivery(Long id);
+
+    /**
+     * 完成订单
+     *
+     * @param id
+     */
+    void complete(Long id);
+
+    void reminder(Long id);
 }
